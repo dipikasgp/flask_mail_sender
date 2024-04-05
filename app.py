976815c5915f8a029b233aa9c9818e0117e52ff5
Dipika Sengupta <senguptadipika30@gmail.com>
2024-04-05 16:47:03 +0200

    Added bulk email functionality and attachments
    Added bulk email functionality and attachment in the email

--- app.py
+++ app.py
@@ -5,12 +5,26 @@ from flask_mail import Message
 @app.route("/send_mail")
 def index():
     mail_content = Message(
-        'Testing Testing 1 2 3....',
+        'Attachment',
         recipients=['XYZ@gmail.com','ABC@gmail.com'])
     mail_content.body = "This is a test"
+    mail_content.html = '<b>Please see attached</b>'
+    with app.open_resource('some_attachment.pdf') as pdf:
+        mail_content.attach('some_attachment.pdf','application/pdf',pdf.read())
     mail.send(mail_content)
     return "Mail has sent"
 
 
+@app.route('/bulk_mail')
+def bulk_mail():
+    users = [{'name':'ABC', 'mail': 'ABC@email.com'}]
+    with mail.connect() as conn:
+        for user in users:
+            msg = Message('Bulk', recipients=[user['mail']])
+            msg.body= f"Hey {user['name']}!"
+            conn.send(msg)
+
 if __name__ == '__main__':
+    with app.app_context():
+        index()
     app.run()
--- config.py
+++ config.py
@@ -4,10 +4,10 @@ app = Flask(__name__)
 
 app.config['MAIL_SERVER']="smtp.office365.com"
 app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = "ABC2@outlook.com"
+app.config['MAIL_USERNAME'] = "ABC@outlook.com"
 app.config['MAIL_PASSWORD'] = "password"
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_DEFAULT_SENDER'] = ('ABC', 'ABC@outlook.com')
-
+app.config['MAIL_MAX_EMAILS'] = 20 #it can send 20 emails at once. then close the connection and run again
 mail = Mail(app)
